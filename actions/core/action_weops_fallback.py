06b98d48c36d363676ea396283884bc7b3464655
umaru <15875339926@139.com>
2023-06-12 15:58:38 +0800

    添加Top3 Intent日志

--- actions/core/action_weops_fallback.py
+++ actions/core/action_weops_fallback.py
@@ -21,7 +21,7 @@ class ActionWeOpsFallback(Action):
         run_mode = server_settings.run_mode
 
         logger.info(f'无法识别用户的意图，进入默认Fallback，用户输入的信息为:{user_msg}')
-
+        logger.info(f'TOP3 Intent结果如下：{tracker.latest_message["intent_ranking"][0:3]}')
         if tracker.active_loop_name is None:
             if run_mode == 'DEV':
                 dispatcher.utter_message(text='OpsPilot当前运行在开发模式，没有办法回复这些复杂的问题哦')
