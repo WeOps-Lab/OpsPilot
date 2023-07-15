1524b6f621d0b6753a890d1a4e3d98600dbe14f3
umaru <15875339926@139.com>
2023-07-15 15:01:05 +0800

    fix online_chat_form_validation.py

--- actions/core/forms/online_chat_form_validation.py
+++ actions/core/forms/online_chat_form_validation.py
@@ -19,10 +19,12 @@ class ValidateOnlineChatForm(FormValidationAction):
             tracker: Tracker,
             domain: DomainDict,
     ) -> Dict[Text, Any]:
-
+        if tracker.get_latest_input_channel() == 'slack':
+            slot_value = slot_value[1:-1]
         if is_valid_url(slot_value):
             return {'online_chat_url': slot_value}
         else:
-            logger.info(f'输入的URL地址不合法,当前输入的地址为:[{slot_value}],当前通道为[{tracker.get_latest_input_channel()}]')
+            logger.info(
+                f'输入的URL地址不合法,当前输入的地址为:[{slot_value}],当前通道为[{tracker.get_latest_input_channel()}]')
             dispatcher.utter_message(f'输入的URL地址不合法,当前输入的地址为:[{slot_value}],请重新输入')
             return {'online_chat_url': None}
