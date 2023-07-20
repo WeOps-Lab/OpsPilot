b78b8a58e36a7327c39363b2a3f53ff044e2b62e
umaru <15875339926@139.com>
2023-07-20 18:40:29 +0800

    新增图数据库导入能力

--- ops_pilot_cli.py
+++ ops_pilot_cli.py
@@ -1,24 +1,36 @@
+import json
 import os
 import shutil
 
 import fire
+import pandas as pd
 from dotenv import load_dotenv
 from langchain import FAISS
-from langchain.document_loaders import PyPDFium2Loader, UnstructuredMarkdownLoader, UnstructuredWordDocumentLoader, \
+from langchain.document_loaders import (
+    PyPDFium2Loader, UnstructuredMarkdownLoader, UnstructuredWordDocumentLoader,
     UnstructuredPowerPointLoader
+)
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import MarkdownTextSplitter, RecursiveCharacterTextSplitter
 from loguru import logger
+from py2neo import Relationship, Node, Graph, NodeMatcher
 from tqdm import tqdm
 
 from actions.constant.server_settings import server_settings
 from actions.utils.indexer_utils import Searcher
-from actions.utils.langchain_utils import langchain_qa
+from actions.utils.langchain_utils import langchain_qa, graph_db_chat
 from actions.utils.redis_utils import RedisUtils
 
 
-class BootStrap(object):
-    def init_data(self, force=False):
+class BootStrap:
+    """
+    主要功能：
+    1. 初始化系统配置
+    2. 进入命令行模式进行本地知识问答
+    3. 索引目标路径下的文件，存放至向量数据库与倒排索引中
+    4. 从CSV和Excel文件中读取实体和关系信息，创建实体和关系节点，并添加到Neo4j数据库中
+    """
+    def init_data(self, force: bool = False):
         """
         初始化系统配置
         Args:
@@ -30,11 +42,11 @@ class BootStrap(object):
         """
         进入命令行模式进行本地知识问答
         """
-        embeddings = HuggingFaceEmbeddings(model_name=server_settings.embed_model_name,
-                                           cache_folder=server_settings.embed_model_cache_home,
-                                           encode_kwargs={
-                                               'show_progress_bar': True
-                                           })
+        embeddings = HuggingFaceEmbeddings(
+            model_name=server_settings.embed_model_name,
+            cache_folder=server_settings.embed_model_cache_home,
+            encode_kwargs={'show_progress_bar': True}
+        )
         doc_search = FAISS.load_local(server_settings.vec_db_path, embeddings)
 
         searcher = Searcher()
@@ -94,19 +106,149 @@ class BootStrap(object):
         knowledge_contents = [x.page_content for x in knowledge_docs]
 
         logger.info('建立知识的语义索引......')
-        embeddings = HuggingFaceEmbeddings(model_name=server_settings.embed_model_name,
-                                           cache_folder=server_settings.embed_model_cache_home,
-                                           encode_kwargs={
-                                               'show_progress_bar': True
-                                           })
+        embeddings = HuggingFaceEmbeddings(
+            model_name=server_settings.embed_model_name,
+            cache_folder=server_settings.embed_model_cache_home,
+            encode_kwargs={'show_progress_bar': True}
+        )
         faiss_db = FAISS.from_documents(knowledge_docs, embeddings)
         faiss_db.save_local(folder_path=server_settings.vec_db_path)
         logger.info('建立知识内容的倒排索引.....')
         search = Searcher()
         search.index_knowledge(knowledge_contents)
 
+    def create_relationships_from_files(self, folder_path: str, json_config_path: str):
+        """
+        从CSV和Excel文件中读取关系信息，创建关系节点，并添加到Neo4j数据库中
+        Args:
+            folder_path: 存放CSV和Excel文件的文件夹路径
+            json_config_path: 存放实体和关系信息的JSON文件路径
+        """
+        # 从JSON配置文件中加载实体和关系的配置信息
+        with open(json_config_path, 'r', encoding='utf-8') as f:
+            config_data = json.load(f)
+
+        relationships_config = config_data.get('relationships', [])
+
+        # 连接Neo4j数据库
+        graph = Graph(server_settings.neo4j_url, auth=(server_settings.neo4j_username, server_settings.neo4j_password))
+
+        # 遍历文件夹中的所有文件，处理csv和excel文件
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+
+            # 判断文件类型是CSV还是Excel
+            if file_name.lower().endswith('.csv'):
+                # 使用Pandas读取CSV文件
+                csv_data = pd.read_csv(file_path)
+            elif file_name.lower().endswith('.xls') or file_name.lower().endswith('.xlsx'):
+                # 使用Pandas读取Excel文件，指定读取引擎为openpyxl
+                csv_data = pd.read_excel(file_path, engine='openpyxl')
+            else:
+                continue
+
+            # 遍历关系配置，创建关系
+            for rel_config in tqdm(relationships_config, desc=f'Importing relationships from {file_name}',
+                                   unit='relationship'):
+                rel_type = rel_config.get('type')
+                start_entity = rel_config.get('start_entity')
+                end_entity = rel_config.get('end_entity')
+                property_mapping = rel_config.get('property_mapping')
+
+                # 使用 filter 方法过滤不存在的列
+                rel_data = csv_data.filter(items=property_mapping.keys(), axis=1).rename(
+                    columns=property_mapping).drop_duplicates()
+
+                # 创建关系并添加到Neo4j数据库
+                for _, row in tqdm(rel_data.iterrows(), total=len(rel_data), desc='Importing relationships',
+                                   unit='relationship'):
+                    start_nodes = graph.nodes.match(start_entity, **row.to_dict())
+                    end_nodes = graph.nodes.match(end_entity, **row.to_dict())
+
+                    for start_node in start_nodes:
+                        for end_node in end_nodes:
+                            relationship = Relationship(start_node, rel_type, end_node)
+                            graph.create(relationship)
+
+                logger.info(f"Imported relationships from {file_name}: {rel_type}")
+
+    def create_entities_from_files(self, folder_path: str, json_config_path: str):
+        """
+        从CSV和Excel文件中读取实体信息，创建实体节点，并添加到Neo4j数据库中
+        Args:
+            folder_path: 存放CSV和Excel文件的文件夹路径
+            json_config_path: 存放实体和关系信息的JSON文件路径
+        """
+        # 从JSON配置文件中加载实体的配置信息
+        with open(json_config_path, 'r', encoding='utf-8') as f:
+            config_data = json.load(f)
+
+        entities_config = config_data['entities']
+
+        # 连接Neo4j数据库
+        graph = Graph(server_settings.neo4j_url, auth=(server_settings.neo4j_username, server_settings.neo4j_password))
+        graph.delete_all()
+
+        missing_columns = []
+        # 遍历文件夹中的所有文件，处理csv和excel文件
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+
+            ext = os.path.splitext(file_name)[-1].lower()
+            if ext == '.csv':
+                csv_data = pd.read_csv(file_path)
+            elif ext in ['.xls', '.xlsx']:
+                csv_data = pd.read_excel(file_path)
+            else:
+                continue
+
+            # 遍历实体配置，创建节点
+            for entity_config in tqdm(entities_config, desc=f'Importing entities from {file_name}', unit='entity'):
+                entity_label = entity_config['label']
+                property_mapping = entity_config['property_mapping']
+
+                # 检查数据列名是否存在
+                missing_columns_for_entity = False
+                for column_name in property_mapping.values():
+                    if column_name not in csv_data.columns:
+                        missing_columns_for_entity = True
+                        break
+                if missing_columns_for_entity:
+                    continue
+
+                # 重命名数据列
+                entity_data = csv_data.rename(columns=property_mapping)
+
+                # 创建实体节点并添加到Neo4j数据库
+                for _, row in tqdm(entity_data.iterrows(), total=len(entity_data), desc='Creating entities',
+                                   unit='entity'):
+                    entity_properties = {key: row.get(property_mapping[key]) for key in property_mapping}
+                    entity_node = Node(entity_label, **entity_properties)
+
+                    # 使用MERGE语句来创建实体节点，确保不会创建重复的实体节点
+                    matcher = NodeMatcher(graph)
+                    existing_node = matcher.match(entity_label, **entity_properties).first()
+                    if existing_node is None:
+                        graph.create(entity_node)
+
+                logger.info(f"Imported entities from {file_name}: {entity_label}")
+
+    def query_graphdb_knowledge(self):
+        """
+        进入命令行模式进行图数据库问题查询
+        """
+        while True:
+            query = input("请输入问题（输入exit退出终端）：")
+            if query == "exit":
+                break
+
+            results = graph_db_chat(query)
+
+            logger.info(f'回复:[{results}]')
+
 
 if __name__ == '__main__':
     load_dotenv()
     os.environ.setdefault('SENTENCE_TRANSFORMERS_HOME', server_settings.embed_model_cache_home)
+
     fire.Fire(BootStrap)
