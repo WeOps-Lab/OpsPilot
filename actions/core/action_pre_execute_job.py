8cf4d11ce81f6e02facf5182613072f5ede3094b
umaru <15875339926@139.com>
2023-07-21 18:28:06 +0800

    [新增]Redis 故障处置预案规则

--- actions/core/action_pre_execute_job.py
+++ actions/core/action_pre_execute_job.py
@@ -6,14 +6,14 @@ from rasa_sdk.executor import CollectingDispatcher
 from actions.utils.core_utils import get_regex_entities
 
 
-class ActionExecuteJob(Action):
+class ActionPreExecuteJob(Action):
 
     def name(self) -> Text:
-        return "action_execute_job"
+        return "action_pre_execute_job"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(
-            f'目标IP:[{tracker.get_slot("ip_address")}],任务:{tracker.get_slot("internal_job_name")},执行完成，执行结果如下:....')
+            f'开始执行任务:目标IP:[{tracker.get_slot("ip_address")}],任务:{tracker.get_slot("internal_job_name")},请稍后...')
         return []
