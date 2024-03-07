a060119beb9fa93177c47fba22f2fae47bc27103
umaru <15875339926@139.com>
2024-03-07 12:16:39 +0800

    add wechat support

--- tests/actions/utils/langchain_utils_test.py
+++ tests/actions/utils/langchain_utils_test.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
-
+from loguru import logger
 from actions.utils.langchain_utils import LangChainUtils
 
 
 def test_chat_llm_with_memory():
     load_dotenv()
     result = LangChainUtils.chat_llm_with_memory('demo', '你是谁')
-    print(result)
+    logger.info(result)
