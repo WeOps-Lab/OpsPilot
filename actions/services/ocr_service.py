0747819b75938e067345092d98b221f9d2c1bef8
umaru <15875339926@139.com>
2024-04-16 14:20:13 +0800

    https://github.com/PaddlePaddle/PaddleOCR/issues/8153   paddleocr issue适配

--- actions/services/ocr_service.py
+++ actions/services/ocr_service.py
@@ -2,11 +2,10 @@ from paddleocr import PaddleOCR, draw_ocr
 
 
 class OcrService:
-    def __init__(self):
-        self.engine = PaddleOCR(use_angle_cls=True, lang="ch")
 
     def extract_content(self, img_path) -> str:
-        result = self.engine.ocr(img_path, cls=True)
+        engine = PaddleOCR(use_angle_cls=True, lang="ch")
+        result = engine.ocr(img_path, cls=True)
         content = ''
         for idx in range(len(result)):
             res = result[idx]
