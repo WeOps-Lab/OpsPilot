eec0a5570404c6e86efa1591bb02d27fa121e121
umaru <15875339926@139.com>
2024-04-21 17:09:17 +0800

    [安全]资产测绘: 输入`资产测绘`，我可以帮助您测绘目标资产，了解IT资产暴露的端口、服务等信息

--- actions/core/action_scan_remider.py
+++ actions/core/action_scan_remider.py
@@ -1,6 +1,6 @@
 from typing import Text, Dict, Any, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, logger
 from rasa_sdk.executor import CollectingDispatcher
 
 from actions.services.kscan_service import KScanService
@@ -26,7 +26,7 @@ class ActionScanReminder(Action):
             (x['value'] for x in entities if x['entity'] == 'scan_targets'),
             None
         )
-
+        logger.info(f'开始扫描:[{scan_targets}]')
         scan_result = service.scan(scan_targets)
         markdown_result = service.json_to_markdown(scan_result)
         dispatcher.utter_message('扫描结果如下：')
