133249156f6b727900b75109b2e6250cda88194c
umaru <15875339926@139.com>
2024-04-22 14:46:30 +0800

    upgrade

--- actions/core/action_scan.py
+++ actions/core/action_scan.py
@@ -2,10 +2,12 @@ import datetime
 import logging
 from typing import Text, Dict, Any, List
 
+from loguru import logger
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet, ReminderScheduled
 from rasa_sdk.executor import CollectingDispatcher
 from tasks.celery import scan_target
+
 
 class ActionScan(Action):
     def __init__(self):
@@ -20,6 +22,7 @@ class ActionScan(Action):
             tracker: Tracker,
             domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
+        logger.info(f'启动扫描资产任务,当前对话通道:{tracker.get_latest_input_channel()}')
         scan_targets = tracker.get_slot("scan_targets")
         dispatcher.utter_message(f"开始对[{scan_targets}]进行资产测绘~,扫描结束后，小助手会第一时间通知你哟")
         scan_target.delay(tracker.sender_id, scan_targets)
