f81bc1c0348f58e2680b9d9703bded035fc1776f
umaru <15875339926@139.com>
2024-05-12 22:00:21 +0800

    add dingtalk support

--- tests/services/autogen_service_test.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from actions.services.autogen_service import AutogenService
-
-
-def test_chat():
-    service = AutogenService()
-    results = service.chat('这是一个什么网站: https://www.oschina.net/')
-    logging.info(results)
