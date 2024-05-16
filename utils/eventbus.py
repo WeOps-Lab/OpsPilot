4f56ea6788176d56eea71830464745d26b4a29a2
umaru <15875339926@139.com>
2024-05-16 16:06:48 +0800

    add eventbus

--- utils/eventbus.py
+++ utils/eventbus.py
@@ -2,32 +2,45 @@ import json
 import threading
 
 import pika
+from loguru import logger
 
 from actions.constants.server_settings import server_settings
 
 
 class EventBus:
-    def __init__(self):
+    def publish(self, messages):
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=server_settings.rabbitmq_host, port=server_settings.rabbitmq_port,
+
                                       credentials=pika.PlainCredentials(server_settings.rabbitmq_username,
                                                                         server_settings.rabbitmq_password)))
-        self.channel = connection.channel()
-        self.channel.exchange_declare(exchange='event_bus', exchange_type='fanout', durable=True)
+        channel = connection.channel()
+        channel.exchange_declare(exchange='event_bus', exchange_type='fanout', durable=True)
 
-    def publish(self, messages):
-        self.channel.basic_publish(exchange='event_bus', routing_key='', body=messages,
-                                   properties=pika.BasicProperties(delivery_mode=2))
+        channel.basic_publish(exchange='event_bus', routing_key='', body=messages,
+                              properties=pika.BasicProperties(delivery_mode=2))
+        connection.close()
 
     def consume(self, queue_name, func):
-        result = self.channel.queue_declare(queue=queue_name)
-        self.channel.queue_bind(exchange='event_bus', queue=queue_name)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=server_settings.rabbitmq_host, port=server_settings.rabbitmq_port,
+
+                                      credentials=pika.PlainCredentials(server_settings.rabbitmq_username,
+                                                                        server_settings.rabbitmq_password)))
+        channel = connection.channel()
+        channel.exchange_declare(exchange='event_bus', exchange_type='fanout', durable=True)
+
+        result = channel.queue_declare(queue=queue_name)
+        channel.queue_bind(exchange='event_bus', queue=queue_name)
 
         def callback(ch, method, properties, body):
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-            obj = json.loads(body.decode())
-            func(obj)
+            try:
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+                obj = json.loads(body.decode())
+                func(obj)
+            except Exception as e:
+                logger.error(e)
 
-        self.channel.basic_consume(result.method.queue, callback, False)
-        consume_thread = threading.Thread(target=self.channel.start_consuming)
+        channel.basic_consume(result.method.queue, callback, False)
+        consume_thread = threading.Thread(target=channel.start_consuming)
         consume_thread.start()
