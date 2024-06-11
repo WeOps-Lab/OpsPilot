b67e1243a99e06555e7418709377962abcaa94f3
umaru <15875339926@139.com>
2024-06-11 16:06:49 +0800

    重构：重构消息处理逻辑和导入语句
    - 移除了对`multiprocessing`的导入语句
    - 向文件中添加了对`Channel`的导入语句
    - `handle`方法被拆分为两个不同的方法
    - 添加了一个新的`on_message`方法
    - 修改了创建用户频道的逻辑
    
    Signed-off-by: umaru <15875339926@139.com>

--- munchkin/apps/bot_mgmt/management/commands/consume_bot_event.py
+++ munchkin/apps/bot_mgmt/management/commands/consume_bot_event.py
@@ -1,13 +1,59 @@
+import datetime
 import json
-import multiprocessing
 
 import pika
 from django.core.management import BaseCommand
 from loguru import logger
+
 from apps.bot_mgmt.models import Bot, BotConversationHistory
-from apps.channel_mgmt.models import ChannelUser, ChannelUserGroup
+from apps.channel_mgmt.models import Channel, ChannelUser, ChannelUserGroup
 from munchkin.components.conversation_mq import CONVERSATION_MQ_HOST, CONVERSATION_MQ_PORT, CONVERSATION_MQ_USER, \
     CONVERSATION_MQ_PASSWORD
+
+user_channels = {}
+
+
+def on_message(channel, method_frame, header_frame, body):
+    try:
+        message = json.loads(body.decode())
+
+        if 'text' in message:
+            logger.debug(f'收到消息:{message}')
+
+            sender_id = message['sender_id']
+            if 'input_channel' in message:
+                input_channel = message['input_channel']
+                user_channels[sender_id] = message['input_channel']
+            else:
+                input_channel = user_channels[sender_id]
+
+            assistant_id = message['metadata']['assistant_id']
+            bot = Bot.objects.get(assistant_id=assistant_id)
+            channel_obj = Channel.objects.get(name=input_channel)
+            channel_user_exists = ChannelUser.objects.filter(user_id=sender_id,
+                                                             channel_user_group__channel=channel_obj).exists()
+            if channel_user_exists is False:
+                logger.info(f'用户[{sender_id}]不存在,创建用户,并加入默认用户组')
+
+                channel_user_group = ChannelUserGroup.objects.get(channel=channel_obj,
+                                                                  name='默认用户组')
+                ChannelUser.objects.create(channel_user_group=channel_user_group,
+                                           user_id=sender_id)
+
+            channel_user = ChannelUser.objects.filter(user_id=sender_id).first()
+
+            # 创建对话历史
+            created_at = datetime.datetime.fromtimestamp(message['timestamp'], tz=datetime.timezone.utc)
+            logger.debug(
+                f'写入消息，完整信息如下: bot={bot}, user={channel_user}, created_at={created_at}, conversation_role={message["event"]}, conversation={message["text"]}')
+
+            BotConversationHistory.objects.get_or_create(bot=bot, user=channel_user,
+                                                         created_at=created_at,
+                                                         conversation_role=message['event'],
+                                                         conversation=message['text'])
+    except Exception as e:
+        logger.error(f'消息处理失败:{e}')
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 
 class Command(BaseCommand):
@@ -24,33 +70,9 @@ class Command(BaseCommand):
             )
         ))
         channel = connection.channel()
-
-        for method_frame, properties, body in channel.consume(f'pilot'):
-            message = json.loads(body.decode())
-            if 'text' in message:
-                sender_id = message['sender_id']
-                input_channel = message['input_channel']
-                assistant_id = message['metadata']['assistant_id']
-
-                bot = Bot.objects.get(assistant_id=assistant_id)
-
-                channel_user_exists = ChannelUser.objects.filter(user_id=sender_id,
-                                                                 channel_user_group__channel=input_channel).exists()
-                if channel_user_exists is False:
-                    logger.info(f'用户[{sender_id}]不存在,创建用户,并加入默认用户组')
-
-                    channel_obj = ChannelUser.objects.filter(user_id=sender_id,
-                                                             channel_user_group__channel=input_channel).first()
-                    channel_user_group = ChannelUserGroup.objects.get(channel=channel_obj,
-                                                                      name='默认用户组')
-                    ChannelUser.objects.create(channel_user_group=channel_user_group,
-                                               user_id=sender_id)
-
-                channel_user = ChannelUser.objects.filter(user_id=sender_id).first()
-
-                # 创建对话历史
-                BotConversationHistory.objects.create(bot=bot, user=channel_user,
-                                                      created_at=message['timestamp'],
-                                                      conversation_role=message['event'],
-                                                      conversation=message['text'])
-            channel.basic_ack(method_frame.delivery_tag)
+        channel.basic_consume('pilot', on_message)
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+        connection.close()
