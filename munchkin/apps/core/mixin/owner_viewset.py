a4364d28ae789bfb162d16570852726bfb072453
umaru <15875339926@139.com>
2024-06-13 18:41:31 +0800

    功能：添加OwnerViewSet以检索和保存用户记录
    - 添加一个名为`munchkin/apps/core/mixin/owner_viewset.py`的新文件，内容如下：
      - 一个名为`OwnerViewSet`的类，继承自多个mixin和`GenericViewSet`
      - `get_queryset`方法返回当前认证用户的记录列表
      - `perform_create`方法将当前认证用户保存为拥有者
    - 添加一个名为`munchkin/apps/core/viewsets/__init__.py`的新文件
    
    Signed-off-by: umaru <15875339926@139.com>

---
+++ munchkin/apps/core/mixin/owner_viewset.py
@@ -0,0 +1,21 @@
+from rest_framework import mixins, viewsets
+from django.db.models import Q
+
+
+class OwnerViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the records
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return self.queryset.filter(Q(owner=user) | Q(is_public=True))
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
