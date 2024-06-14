8d580e81dde6cf0b827972ce6f78fae35966f0ec
umaru <15875339926@139.com>
2024-06-14 18:53:35 +0800

    功能: 添加新的提供者，用于重新排序和嵌入模型
    - 添加一个名为`bce-reranker-base_v1`的RerankProvider，具有`BCE`的rerank_model和`./models/bce-reranker-base_v1`的rerank_config模型
    - 添加一个名为`bce-embedding-base_v1`的EmbedProvider，具有`BCEEMBEDDING`的embed_model和`./models/bce-embedding-base_v1`的embed_config模型
    - 添加一个名为`text-embedding-ada-002`的EmbedProvider，具有`OPENAI`的embed_model和`text-embedding-ada-002`的embed_config模型
    
    这个git diff将新的提供者及其相应的配置模型添加到`init_llm.py`文件中。
    
    Signed-off-by: umaru <15875339926@139.com>

--- munchkin/apps/model_provider_mgmt/management/commands/init_llm.py
+++ munchkin/apps/model_provider_mgmt/management/commands/init_llm.py
@@ -8,30 +8,31 @@ class Command(BaseCommand):
     help = '初始化模型数据'
 
     def handle(self, *args, **options):
-        obj, created = RerankProvider.objects.get_or_create(name='bce-reranker-base_v1',
-                                                            rerank_model=RerankModelChoices.BCE)
-        if created:
-            obj.rerank_config = {
-                'model': './models/bce-reranker-base_v1',
-            }
-            obj.save()
-        obj, created = EmbedProvider.objects.get_or_create(name='bce-embedding-base_v1',
-                                                           embed_model=EmbedModelChoices.BCEEMBEDDING)
-        if created:
-            obj.embed_config = {
-                'model': './models/bce-embedding-base_v1',
-            }
-            obj.save()
+        RerankProvider.objects.get_or_create(name='bce-reranker-base_v1',
+                                             rerank_model=RerankModelChoices.BCE,
+                                             defaults={
+                                                 'rerank_config': {
+                                                     'model': './models/bce-reranker-base_v1'
+                                                 }
+                                             })
 
-        obj, created = EmbedProvider.objects.get_or_create(name='text-embedding-ada-002',
-                                                           embed_model=EmbedModelChoices.OPENAI)
-        if created:
-            obj.embed_config = {
-                'model': 'text-embedding-ada-002',
-                'openai_api_key': 'your_openai_api_key',
-                'openai_base_url': 'https://api.openai.com',
-            }
-            obj.save()
+        EmbedProvider.objects.get_or_create(name='bce-embedding-base_v1',
+                                            embed_model=EmbedModelChoices.BCEEMBEDDING,
+                                            defaults={
+                                                'embed_config': {
+                                                    'model': './models/bce-embedding-base_v1',
+                                                }
+                                            })
+
+        EmbedProvider.objects.get_or_create(name='text-embedding-ada-002',
+                                            embed_model=EmbedModelChoices.OPENAI,
+                                            defaults={
+                                                'embed_config': {
+                                                    'model': 'text-embedding-ada-002',
+                                                    'openai_api_key': 'your_openai_api_key',
+                                                    'openai_base_url': 'https://api.openai.com',
+                                                }
+                                            })
 
         EmbedProvider.objects.get_or_create(
             name='FastEmbed(BAAI/bge-small-en-v1.5)',
@@ -50,31 +51,31 @@ class Command(BaseCommand):
             enabled=True
         )
 
-        llm_model, created = LLMModel.objects.get_or_create(
+        LLMModel.objects.get_or_create(
             name='GPT-4 32K',
             llm_model=LLMModelChoices.CHAT_GPT,
-        )
-        if created:
-            llm_model.llm_config = {
-                'openai_api_key': 'your_openai_api_key',
-                'openai_base_url': 'https://api.openai.com',
-                'temperature': 0.7,
-                'model': 'gpt-4-32k',
+            defaults={
+                'llm_config': {
+                    'openai_api_key': 'your_openai_api_key',
+                    'openai_base_url': 'https://api.openai.com',
+                    'temperature': 0.7,
+                    'model': 'gpt-4-32k',
+                }
             }
-            llm_model.save()
+        )
 
         llm_model, created = LLMModel.objects.get_or_create(
             name='GPT-3.5 Turbo 16K',
             llm_model=LLMModelChoices.CHAT_GPT,
-        )
-        if created:
-            llm_model.llm_config = {
-                'openai_api_key': 'your_openai_api_key',
-                'openai_base_url': 'https://api.openai.com',
-                'model': 'gpt-3.5-turbo-16k',
-                'temperature': 0.7,
+            defaults={
+                'llm_config': {
+                    'openai_api_key': 'your_openai_api_key',
+                    'openai_base_url': 'https://api.openai.com',
+                    'temperature': 0.7,
+                    'model': 'gpt-3.5-turbo-16k',
+                }
             }
-            llm_model.save()
+        )
 
         prompt = """
 参考信息：
@@ -88,12 +89,12 @@ class Command(BaseCommand):
 回答一定要忠于原文，简洁但不丢信息，不要胡乱编造。我的问题或指令是什么语种，你就用什么语种回复,
 你的回复：                                 
                 """
-        llm_skill, created = LLMSkill.objects.get_or_create(
+        LLMSkill.objects.get_or_create(
             name='开放问答(GPT3.5-16k)',
             llm_model=llm_model,
             skill_id='action_llm_fallback',
             enable_conversation_history=True,
+            defaults={
+                'skill_prompt': prompt
+            }
         )
-        if created:
-            llm_skill.skill_prompt = prompt
-            llm_skill.save()
