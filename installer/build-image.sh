439c4386aa771ca73f4aa62e912a717dc2f69549
umaru <15875339926@139.com>
2024-06-20 16:40:53 +0800

    构建：重新构建Docker和服务配置文件以适应 `fast_embed_server`
    - 为 `fast_embed_server` 目录添加一个Dockerfile
    - 为 `fast_embed_server` 添加一个服务配置文件
    - 修复 `pilot-base` 的Docker构建命令中的拼写错误
    
    Signed-off-by: umaru <15875339926@139.com>

--- installer/build-image.sh
+++ installer/build-image.sh
@@ -1,1 +1,4 @@
 docker build -t ccr.ccs.tencentyun.com/megalab/pilot-base -f ./support-files/docker/Dockerfile.base .
+
+cd ./model_server/fast_embed_server
+docker build -t ccr.ccs.tencentyun.com/megalab/fast-embed-server .
