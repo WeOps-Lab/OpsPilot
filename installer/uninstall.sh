a975f787138dfb8dc6d4533f4a9d0b13e93d40ef
umaru <15875339926@139.com>
2024-06-20 16:00:16 +0800

    事务：重构安装程序的 Kubernetes 部署文件
    - 将 `support-files/k8s/elasticsearch.yml` 重命名为 `installer/depend/elasticsearch.yml`
    - 从 `support-files/k8s/elasticsearch.yml` 中删除 `type: NodePort`
    - 从 `support-files/k8s/elasticsearch.yml` 中删除 `nodePort: 30006`
    - 将 `support-files/k8s/minio.yml` 重命名为 `installer/depend/minio.yml`
    - 从 `support-files/k8s/minio.yml` 中删除 `type: NodePort`
    - 从 `support-files/k8s/minio.yml` 中删除 `nodePort: 30004`
    - 将 `support-files/k8s/postgres.yml` 重命名为 `installer/depend/postgres.yml`
    - 从 `support-files/k8s/postgres.yml` 中删除 `type: NodePort`
    - 从 `support-files/k8s/postgres.yml` 中删除 `nodePort: 30003`
    - 将 `support-files/k8s/rabbitmq.yml` 重命名为 `installer/depend/rabbitmq.yml`
    - 从 `support-files/k8s/rabbitmq.yml` 中删除 `type: NodePort`
    - 从 `support-files/k8s/rabbitmq.yml` 中删除 `nodePort: 30001
    
    Signed-off-by: umaru <15875339926@139.com>

--- installer/uninstall.sh
+++ installer/uninstall.sh
@@ -1,6 +1,6 @@
 /usr/local/bin/k3s-uninstall.sh
 systemctl stop docker
 systemctl disable --now docker
-apt-get purge docker-ce docker-ce-cli containerd.io -y
+apt-get purge docker* containerd.io -y
 rm -rf /var/lib/docker
 rm -Rf /var/lib/containerd/
