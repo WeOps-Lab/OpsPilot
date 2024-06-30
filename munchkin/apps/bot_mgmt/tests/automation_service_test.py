3d11ce7bbd7c1cb5a81c9bbd73f38ee2028283d6
umaru <15875339926@139.com>
2024-06-30 17:56:27 +0800

    remove salt

--- munchkin/apps/bot_mgmt/tests/automation_service_test.py
+++
@@ -1,8 +0,0 @@
-from apps.bot_mgmt.services.automation_service import AutomationService
-from loguru import logger
-
-
-def test_salt_runner():
-    service = AutomationService()
-    result = service.execute_salt_local('cmd.run', '*', 'pwd')
-    logger.info(result)
