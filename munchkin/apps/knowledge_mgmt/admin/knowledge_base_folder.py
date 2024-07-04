24efeae58d8e887d7d9f54ee1130257d0a429537
umaru <15875339926@139.com>
2024-07-04 10:50:16 +0800

    功能: 在FileKnowledge的表单中添加"Excel解析"部分
    - 在file_knowledge.py中的FileKnowledgeAdmin表单中添加"Excel解析"部分
    - 在knowledge_base_folder.py中的FileKnowledgeInline表单中添加"Excel解析"部分
    
    Signed-off-by: umaru <15875339926@139.com>

--- munchkin/apps/knowledge_mgmt/admin/knowledge_base_folder.py
+++ munchkin/apps/knowledge_mgmt/admin/knowledge_base_folder.py
@@ -19,6 +19,8 @@ class FileKnowledgeInline(admin.TabularInline):
                   {"fields": ("enable_general_parse", ("general_parse_chunk_size", "general_parse_chunk_overlap"))}),
                  ("语义分块解析",
                   {"fields": ("enable_semantic_chunck_parse", "semantic_chunk_parse_embedding_model")}),
+                 ("Excel解析",
+                  {"fields": ("excel_header_row_parse", "excel_full_content_parse")}),
                  )
     extra = 0
     readonly_fields = ["title"]
