cd79eb8f31302411de76b04c00f1d51060a0d2b8
umaru <15875339926@139.com>
2024-07-06 23:02:32 +0800

    重构：更新 Dockerfile 和 FastAPI 应用以改进日志记录和错误处理
    - 从 Dockerfile 中删除 apt-get update 和 clean 命令
    - 从 Dockerfile 中删除 pip install -r requirements.in 命令
    - 在 service.conf 中添加 stdout_logfile 和 stdout_logfile_maxbytes 选项
    - 在 server.py 中添加 loguru 和 dotenv 导入
    - 在 FastAPI 应用中添加 CORS 中间件
    - 在 convert 端点中为不支持的转换格式添加警告日志
    - 在 convert 端点中删除删除文件的子进程命令
    - 在 convert 端点中添加异常处理和错误日志记录，以处理文件转换失败的情况
    
    Signed-off-by: umaru <15875339926@139.com>

--- model_server/pandoc_server/server.py
+++ model_server/pandoc_server/server.py
@@ -4,19 +4,35 @@ import subprocess
 import uuid
 from typing import Optional
 
+from dotenv import load_dotenv
 from fastapi import FastAPI, File, Form
 from fastapi import UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
+from loguru import logger
+from starlette.middleware.cors import CORSMiddleware
+
+load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
 
 @app.post("/convert")
 async def convert(output: Optional[str] = Form(...), file: UploadFile = File(...)):
     if output not in ["docx", "pdf"]:
+        logger.warning(f"不支持的转换格式: {output}")
         raise HTTPException(status_code=400, detail="不支持的转换格式")
 
     file_name = str(uuid.uuid4())
+
     with open(file_name, "wb") as buffer:
         buffer.write(await file.read())
 
@@ -44,17 +60,20 @@ async def convert(output: Optional[str] = Form(...), file: UploadFile = File(...
             )
         else:
             subprocess.run(["pandoc", file_name, "-o", f"{file_name}.{output}"], check=True)
-            os.remove(file_name)
-    except subprocess.CalledProcessError:
-        raise HTTPException(status_code=500, detail="转换失败")
+
+        with open(f"{file_name}.{output}", "rb") as f:
+            data = io.BytesIO(f.read())
+        os.remove(f"{file_name}.{output}")
+
+        # 假如转换成功则删除文件，转换失败则保留文件，方便排查原因
+        os.remove(file_name)
         
+        return StreamingResponse(data, media_type="application/octet-stream",
+                                 headers={'Content-Disposition': f'attachment; filename={file_name}.{output}'})
 
-    with open(f"{file_name}.{output}", "rb") as f:
-        data = io.BytesIO(f.read())
-    os.remove(f"{file_name}.{output}")
-
-    return StreamingResponse(data, media_type="application/octet-stream",
-                             headers={'Content-Disposition': f'attachment; filename={file_name}.{output}'})
+    except Exception as e:
+        logger.error(f"文件[{file_name}] 转换失败: {e}")
+        raise HTTPException(status_code=500, detail="转换失败")
 
 
 if __name__ == "__main__":
