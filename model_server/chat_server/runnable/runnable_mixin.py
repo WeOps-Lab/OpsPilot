e24416eadaae6fc3ceb279d256edd8c227a28b28
umaru <15875339926@139.com>
2024-07-06 17:12:29 +0800

    feat: 通过引入`loguru`库并改进`forward_message`方法来提高日志功能
    - 在文件中添加`loguru`库的引入
    - 更新`forward_message`方法以记录额外信息
    
    Signed-off-by: umaru <15875339926@139.com>

--- model_server/chat_server/runnable/runnable_mixin.py
+++ model_server/chat_server/runnable/runnable_mixin.py
@@ -1,6 +1,7 @@
 from langchain.memory import ChatMessageHistory
 
 from user_types.base_chat_request import BaseChatRequest
+from loguru import logger
 
 
 class RunnableMixin:
@@ -21,11 +22,27 @@ class RunnableMixin:
                 window_size=req.conversation_window_size,
                 rag_content=req.rag_context,
             )
+            logger.info(
+                f"""
+                对轮对话模式 
+                系统信息: {req.system_message_prompt} 
+                引用知识:{req.rag_context} 
+                用户信息: {req.user_message}   
+                结果: {result}
+                """)
             return result
         else:
             system_skill_prompt = req.system_message_prompt.replace("{", "").replace("}", "")
             result = driver.chat(
                 system_message_prompt=system_skill_prompt,
                 user_message=req.user_message,
+            )
+            logger.info(
+                f"""
+                单轮对话模式
+                系统信息: {system_skill_prompt}
+                用户信息: {req.user_message}
+                结果: {result}
+                """
             )
             return result
