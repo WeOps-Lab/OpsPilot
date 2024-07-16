e034bb3011f7fdcaae8846cc565f930da5bf91fb
umaru <15875339926@139.com>
2024-07-16 18:10:49 +0800

    任务：从`AzureOcrRequest`类中删除`azure_ocr_key`字段
    - 在`model_server/ocr_server/user_types/azure_ocr_request.py`中删除`AzureOcrRequest`类的`azure_ocr_key`字段
    
    Signed-off-by: umaru <15875339926@139.com>

--- model_server/ocr_server/user_types/azure_ocr_request.py
+++ model_server/ocr_server/user_types/azure_ocr_request.py
@@ -3,6 +3,4 @@ from pydantic.v1 import Field
 
 
 class AzureOcrRequest(CustomUserType):
-    azure_ocr_endpoint: str = Field(..., extra={"widget": {"type": "text"}})
-    azure_ocr_key: str = Field(..., extra={"widget": {"type": "text"}})
     file: str = Field(..., extra={"widget": {"type": "base64file"}})
