b3e5fb1cdbf0839b902be3ba40aada8438ad95cd
ZakiHo <497920432@qq.com>
2024-08-26 11:30:19 +0800

    添加: 文件知识接口添加知识库过滤参数

--- munchkin/apps/knowledge_mgmt/views/file_knowledge_view.py
+++ munchkin/apps/knowledge_mgmt/views/file_knowledge_view.py
@@ -11,7 +11,12 @@ class FileKnowledgeViewSet(GuardianModelViewSet):
     def get_queryset(self):
         queryset = FileKnowledge.objects.all()
         title_exclude = self.request.GET.get("title_exclude", "")
+        knowledge_base_folder_ids = self.request.GET.get("knowledge_base_folder_ids", [])
         if title_exclude:
             queryset = queryset.exclude(title__contains=title_exclude)
+        if knowledge_base_folder_ids:
+            if isinstance(knowledge_base_folder_ids, str):
+                knowledge_base_folder_ids = [knowledge_base_folder_ids]
+            queryset = queryset.filter(knowledge_base_folder__in=knowledge_base_folder_ids)
 
         return queryset
